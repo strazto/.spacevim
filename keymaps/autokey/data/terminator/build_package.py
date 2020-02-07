933d450b160862ba4cdf130d46f749c1c0c72e77
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-02-07 16:42:38 +1100

    Added autokey keymaps for sending to vim

---
+++ keymaps/autokey/data/terminator/build_package.py
@@ -0,0 +1,3 @@
+# ctrl+shift+b sends '<S-F1>'b
+# In vim this will map to build current project
+keyboard.send_keys("<shift>+<f1>+b")
---
+++ keymaps/autokey/data/terminator/test_package.py
@@ -0,0 +1,4 @@
+# ctrl+shift+t sends '<S-F1>'t
+# In vim this will map to test current project
+# ie- devtools::test()
+keyboard.send_keys("<shift>+<f1>+t")
